ea8322860de06475e237be55ece3bce8ddbed4ee
Qixian Guan <gtina7861@gmail.com>
2026-05-06 10:46:56 +0200

    sql para crear las tablas y columnas
    sql para crear tablas y las columnas que necesitamos

---
+++ db/crearTablasYcolumnas.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE IF NOT EXISTS alimentacion;
+USE alimentacion;
+
+CREATE TABLE alimento (
+   id_alimento INT AUTO_INCREMENT PRIMARY KEY,
+   nombre VARCHAR(255) NOT NULL,
+   descripcion TEXT NOT NULL,
+   imagen_url VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE tipo_alimento(
+   id_tipo INT AUTO_INCREMENT PRIMARY KEY,
+   nombre VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE contenido (
+   id_contenido INT AUTO_INCREMENT PRIMARY KEY,
+   texto TEXT NOT NULL,
+   id_tipo INT NOT NULL,
+   id_alimento INT NOT NULL,
+
+  FOREIGN KEY (id_tipo) REFERENCES tipo_alimento(id_tipo),
+  FOREIGN KEY (id_alimento) REFERENCES alimento(id_alimento)
+);
+
